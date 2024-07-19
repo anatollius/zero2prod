bec90610ddaf2c28d1cda9822c2edaa1dab82dda
Nathan Jones <nathan.jones@creditsafe.com>
2024-07-19 20:03:55 +0100

    ch 3 completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::PgPool;
+use std::io::Result;
+use std::net::TcpListener;
+use tokio;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Panic if we can't read the configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to create a Postgres connection");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
