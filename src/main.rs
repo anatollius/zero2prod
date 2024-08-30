27d442b32bc39fdac301c0c012474d5087e7821b
Nathan Jones <nathan.jones@creditsafe.com>
2024-08-30 17:28:05 +0100

    ch4 completed - Telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::io::Result;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let subscriber = get_subscriber("zero2prod", "info", std::io::stdout);
+    init_subscriber(subscriber);
+
     // Panic if we can't read the configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to create a Postgres connection");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to create a Postgres connection");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
--- src/startup.rs
+++ src/startup.rs
@@ -4,12 +4,14 @@ use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::io::Result;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server> {
     let db_pool = web::Data::new(db_pool);
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
