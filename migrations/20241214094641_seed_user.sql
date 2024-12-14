d0042cf28d3dd74f844e5990e5cd838076e2a467
Nathan Jones <nathan.jones@creditsafe.com>
2024-12-14 16:04:17 +0000

    create seed user

---
+++ migrations/20241214094641_seed_user.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (user_id, username, password_hash) VALUES (
+    '98f39fce-665d-4e04-a745-199126534842',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$kXQ+bntWdySeWckLAhGWLA$dd/lB9XdvzjY7IJWJHQoFpZ9JMm+SdAr85PelkrErE8'
+)
