ec851fa9059293dcbe322ee738109aedc2d73ddf
Nathan Jones <nathan.jones@creditsafe.com>
2024-12-20 23:29:47 +0000

    make idempotency work for concurrent requests

--- src/routes/admin/newsletters/post.rs
+++ src/routes/admin/newsletters/post.rs
@@ -1,7 +1,7 @@
 use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
-use crate::idempotency::{get_saved_response, save_response, IdempotencyKey};
+use crate::idempotency::{save_response, try_processing, IdempotencyKey, NextAction};
 use crate::utils::{e400, e500, see_other};
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
@@ -35,20 +35,20 @@ pub async fn publish_newsletter(
     } = form.0;
     let idempotency_key = IdempotencyKey::try_from(idempotency_key).map_err(e400)?;
 
-    if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, **user_id)
+    let transaction = match try_processing(&pool, &idempotency_key, **user_id)
         .await
         .map_err(e500)?
     {
-        FlashMessage::info("The newsletter issue has been published!").send();
-        return Ok(saved_response);
-    }
-
-    match validate_newsletter(&title, &html_content, &text_content) {
-        Err(e) => {
-            FlashMessage::error(e.to_string()).send();
-            return Ok(see_other("/admin/newsletters"));
+        NextAction::StartProcessing(t) => t,
+        NextAction::ReturnSavedResponse(saved_response) => {
+            success_message().send();
+            return Ok(saved_response);
         }
-        _ => {}
+    };
+
+    if let Err(e) = validate_newsletter(&title, &html_content, &text_content) {
+        FlashMessage::error(e.to_string()).send();
+        return Ok(see_other("/admin/newsletters"));
     };
 
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
@@ -67,20 +67,20 @@ pub async fn publish_newsletter(
         }
     }
 
-    FlashMessage::info("The newsletter issue has been published!").send();
+    success_message().send();
     let response = see_other("/admin/newsletters");
-    let response = save_response(&pool, &idempotency_key, **user_id, response)
+    let response = save_response(transaction, &idempotency_key, **user_id, response)
         .await
         .map_err(e500)?;
     Ok(response)
 }
 
 fn validate_newsletter(title: &str, html: &str, text: &str) -> Result<(), anyhow::Error> {
-    if title.len() == 0 {
+    if title.is_empty() {
         Err(anyhow::anyhow!("Title must not be empty"))
-    } else if html.len() == 0 {
+    } else if html.is_empty() {
         Err(anyhow::anyhow!("HTML content must not be empty"))
-    } else if text.len() == 0 {
+    } else if text.is_empty() {
         Err(anyhow::anyhow!("Text content must not be empty"))
     } else {
         Ok(())
@@ -105,4 +105,8 @@ async fn get_confirmed_subscribers(
         .collect();
 
     Ok(confirmed_subscribers)
+}
+
+fn success_message() -> FlashMessage {
+    FlashMessage::info("The newsletter issue has been published!")
 }
